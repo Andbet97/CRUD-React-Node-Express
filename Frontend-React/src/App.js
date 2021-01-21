4272ad9235bb9a72f651ce3690437b194ec74687
Andres Betancurt <andbet050197@gmail.com>
2021-01-20 23:37:48 -0500

    Add styles and start ClientList component

--- Frontend-React/src/App.js
+++ Frontend-React/src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { ClientList } from "./components/ClientsList";
 import { CreateClient } from "./components/CreateClient";
@@ -13,13 +14,16 @@ import { UpdateClient } from "./components/UpdateClient";
 function App() {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Redirect exact from="/" to="/client/list" />
-          <Route path="/client/list" component={ClientList} />
-          <Route path="/client/create" component={CreateClient} />
-          <Route path="/client/edit/:id" component={UpdateClient} />
-        </Switch>
+      <div className="container">
+        <div id="bar"></div>
+        <div className="container" id="main-container">
+          <Switch>
+            <Redirect exact from="/" to="/client/list" />
+            <Route path="/client/list" component={ClientList} />
+            <Route path="/client/create" component={CreateClient} />
+            <Route path="/client/edit/:id" component={UpdateClient} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
--- Frontend-React/src/components/ClientsList.js
+++ Frontend-React/src/components/ClientsList.js
@@ -1,5 +1,55 @@
-import React from 'react';
+import '../css/ClientsList.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
+import { FormControl, OutlinedInput, InputAdornment } from '@material-ui/core';
 
-export const ClientList = () => (
-    <h1>List clients!</h1>
-)
+export const ClientList = () => {
+
+    const [querySearch, setQuerySearch] = useState('');
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        console.log(querySearch);
+    }
+
+    return (
+        <Fragment>
+            <div className="row">
+                <div className="col-6 col-md-9 col-lg-10">
+                    <h1>Listado clientes</h1>
+                </div>
+                <div className="col-6 col-md-3 col-lg-2">
+                    <Link to="/client/create" id="new-client" type="button" className="btn btn-primary">
+                        <AddIcon />
+                        Nuevo cliente
+                    </Link>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 content-box">
+                    <div className="row">
+                        <div className="col-10 offset-1">
+                            <form id="search-form" onSubmit={handleSearch}>
+                                <FormControl fullWidth variant="outlined">
+                                    <OutlinedInput
+                                        id="search"
+                                        startAdornment={
+                                            <InputAdornment position="start">
+                                                <SearchIcon />
+                                            </InputAdornment>
+                                        }
+                                        value={querySearch}
+                                        onChange={(e) => setQuerySearch(e.target.value)}
+                                        placeholder="Buscar"
+                                    />
+                                </FormControl>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
