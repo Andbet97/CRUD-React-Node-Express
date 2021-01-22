9106b7464b227906b5d06f70d4f6e8d89471306b
Andres Betancurt <andbet050197@gmail.com>
2021-01-22 11:38:16 -0500

    add public

--- Backend-NodeExpress/src/app.js
+++ Backend-NodeExpress/src/app.js
@@ -9,6 +9,7 @@ app.set('port', process.env.PORT || 5000);
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // routes
 app.use('/api/clients', require('./routes/clients'));
